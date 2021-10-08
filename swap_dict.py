43489b5b81ee9f7136d38c1fe4a5ced85f25dabb
Rishabh Jain <52294621+Rish8955@users.noreply.github.com>
2021-10-08 16:48:45 +0530

    Update swap_dict.py

--- swap_dict.py
+++ swap_dict.py
@@ -13,3 +13,30 @@ print(dict)
 
 #{'a': 1, 'c': 3, 'b': 2}
 #{1: 'a', 2: 'b', 3: 'c'}
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Python Program to Swap dictionary item’s position
+
+# initializing dictionary
+test_dict = {'Gfg': 4, 'is': 1, 'best': 8, 'for': 10, 'geeks': 9}
+
+# printing original dictionary
+print("The original dictionary is : " + str(test_dict))
+
+# initializing swap indices
+i, j = 1, 3
+
+# conversion to tuples
+tups = list(test_dict.items())
+
+# swapping by indices
+tups[i], tups[j] = tups[j], tups[i]
+
+# converting back
+res = dict(tups)
+
+# printing result
+print("The swapped dictionary : " + str(res))
+
